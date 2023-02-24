0825b0c5ebe4f6dafc0c5a0c0525982428376010
mithem <mithem@github.com>
2023-02-24 08:07:05 +0100

    Revert install.py to shell script
    (is just one file to install anyway)

---
+++ install.sh
@@ -0,0 +1,2 @@
+sh -c "mkdir -p '$HOME/Library/Application Support/LibreOffice/4/user/Scripts/python'"
+sh -c "ln 'acquisitions.py' '$HOME/Library/Application Support/LibreOffice/4/user/Scripts/python'"
