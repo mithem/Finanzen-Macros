a663f70980faf0cc06e17bf6b94fd55cdd2de03d
mithem <mithem@github.com>
2023-04-02 08:47:33 +0200

    Sequential acquisition modes

--- test_acquisitions.py
+++ test_acquisitions.py
@@ -1,5 +1,17 @@
-from datetime import date
-from acquisitions import BaseAcquisition, BasePlanning
+from datetime import date, timedelta
+from acquisitions import (
+    BaseAcquisition,
+    BasePlanningWeightedMonthlyContribution,
+    BasePlanningDatedSequentialAcquisition,
+    BasePlanningWeightedSequentialAcquisition,
+)
+
+
+def ac(name: str, target_budget: float, weight: int, days: int) -> BaseAcquisition:
+    anchor = date(2023, 1, 1)
+    return BaseAcquisition(
+        name, 0, target_budget, anchor + timedelta(days=days), weight
+    )
 
 
 def _sum_acquired_budgets(*acquisitions: BaseAcquisition):
@@ -28,7 +40,7 @@ def test_acquisition():
     assert acquisition.budget_acquired == 50
 
 
-def test_planning_allocate_budget_single_acquisition():
+def test_wmcplanning_allocate_budget_single_acquisition():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=0,
@@ -37,14 +49,14 @@ def test_planning_allocate_budget_single_acquisition():
         weight=1,
     )
     today = date(2023, 1, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition], monthly_budget=10, start_budget=0, today=today
     )
     planning.allocate_budget(planning.monthly_budget, date(2023, 1, 1), 1)
     assert acquisition.budget_acquired == 10
 
 
-def test_planning_calculate_acquired_budgets_doesnt_start_on_today():
+def test_wmcplanning_calculate_acquired_budgets_doesnt_start_on_today():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=0,
@@ -53,14 +65,14 @@ def test_planning_calculate_acquired_budgets_doesnt_start_on_today():
         weight=1,
     )
     today = date(2022, 1, 1)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition], monthly_budget=10, start_budget=0, today=today
     )
     planning.calculate_acquired_budgets()
     assert acquisition.budget_acquired == 0
 
 
-def test_planning_calculate_acquired_budgets_does_start_on_first_day_in_past():
+def test_wmcplanning_calculate_acquired_budgets_does_start_on_first_day_in_past():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=0,
@@ -69,14 +81,14 @@ def test_planning_calculate_acquired_budgets_does_start_on_first_day_in_past():
         weight=1,
     )
     today = date(2023, 1, 2)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition], monthly_budget=10, start_budget=0, today=today
     )
     planning.calculate_acquired_budgets()
     assert acquisition.budget_acquired == 10
 
 
-def test_planning_calculate_acquired_budgets_does_start_on_first_day_when_after_start_date():
+def test_wmcplanning_calculate_acquired_budgets_does_start_on_first_day_when_after_start_date():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=0,
@@ -85,14 +97,14 @@ def test_planning_calculate_acquired_budgets_does_start_on_first_day_when_after_
         weight=1,
     )
     today = date(2023, 2, 1)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition], monthly_budget=10, start_budget=0, today=today
     )
     planning.calculate_acquired_budgets()
     assert acquisition.budget_acquired == 10
 
 
-def test_planning_calculate_acquired_budgets_single_acquisition_before_start():
+def test_wmcplanning_calculate_acquired_budgets_single_acquisition_before_start():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=0,
@@ -101,14 +113,14 @@ def test_planning_calculate_acquired_budgets_single_acquisition_before_start():
         weight=1,
     )
     today = date(2022, 12, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition], monthly_budget=10, start_budget=0, today=today
     )
     planning.calculate_acquired_budgets()
     assert acquisition.budget_acquired == 0
 
 
-def test_planning_calculate_acquired_budgets_single_acquisition_interim():
+def test_wmcplanning_calculate_acquired_budgets_single_acquisition_interim():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=0,
@@ -117,14 +129,14 @@ def test_planning_calculate_acquired_budgets_single_acquisition_interim():
         weight=1,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition], monthly_budget=10, start_budget=0, today=today
     )
     planning.calculate_acquired_budgets()
     assert acquisition.budget_acquired == 30
 
 
-def test_planning_calculate_acquired_budgets_single_acquisition_after_end():
+def test_wmcplanning_calculate_acquired_budgets_single_acquisition_after_end():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=0,
@@ -133,14 +145,14 @@ def test_planning_calculate_acquired_budgets_single_acquisition_after_end():
         weight=1,
     )
     today = date(2023, 6, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition], monthly_budget=20, start_budget=0, today=today
     )
     planning.calculate_acquired_budgets()
     assert acquisition.budget_acquired == 100
 
 
-def test_planning_calculate_acquired_budgets_two_acquisitions_before_start():
+def test_wmcplanning_calculate_acquired_budgets_two_acquisitions_before_start():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -156,7 +168,7 @@ def test_planning_calculate_acquired_budgets_two_acquisitions_before_start():
         weight=1,
     )
     today = date(2022, 12, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2],
         monthly_budget=10,
         start_budget=0,
@@ -167,7 +179,7 @@ def test_planning_calculate_acquired_budgets_two_acquisitions_before_start():
     assert acquisition2.budget_acquired == 0
 
 
-def test_planning_calculate_acquired_budgets_two_acquisitions_interim():
+def test_wmcplanning_calculate_acquired_budgets_two_acquisitions_interim():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -183,7 +195,7 @@ def test_planning_calculate_acquired_budgets_two_acquisitions_interim():
         weight=3,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2],
         monthly_budget=10,
         start_budget=0,
@@ -194,7 +206,7 @@ def test_planning_calculate_acquired_budgets_two_acquisitions_interim():
     assert acquisition2.budget_acquired == 22.5
 
 
-def test_planning_calculate_acquired_budgets_two_acquisitions_after_end_uses_extra_budget():
+def test_wmcplanning_calculate_acquired_budgets_two_acquisitions_after_end_uses_extra_budget():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -210,7 +222,7 @@ def test_planning_calculate_acquired_budgets_two_acquisitions_after_end_uses_ext
         weight=3,
     )
     today = date(2023, 7, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2],
         monthly_budget=40,
         start_budget=0,
@@ -221,7 +233,7 @@ def test_planning_calculate_acquired_budgets_two_acquisitions_after_end_uses_ext
     assert acquisition2.budget_acquired == 220
 
 
-def test_planning_calculate_acquired_budgets_three_acquisitions_interim_uses_extra_budget():
+def test_wmcplanning_calculate_acquired_budgets_three_acquisitions_interim_uses_extra_budget():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -244,7 +256,7 @@ def test_planning_calculate_acquired_budgets_three_acquisitions_interim_uses_ext
         weight=3,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2, acquisition3],
         monthly_budget=60,
         start_budget=0,
@@ -256,7 +268,7 @@ def test_planning_calculate_acquired_budgets_three_acquisitions_interim_uses_ext
     assert acquisition3.budget_acquired == 90 + 7.5
 
 
-def test_planning_calculate_acquired_budgets_three_acquisitions_interim():
+def test_wmcplanning_calculate_acquired_budgets_three_acquisitions_interim():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -279,7 +291,7 @@ def test_planning_calculate_acquired_budgets_three_acquisitions_interim():
         weight=3,
     )
     today = date(2023, 4, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2, acquisition3],
         monthly_budget=60,
         start_budget=0,
@@ -293,7 +305,7 @@ def test_planning_calculate_acquired_budgets_three_acquisitions_interim():
     assert acquisition3.budget_acquired == march + april  # 150
 
 
-def test_planning_calculate_acquired_budgets_three_acquisitions_after_end():
+def test_wmcplanning_calculate_acquired_budgets_three_acquisitions_after_end():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -316,7 +328,7 @@ def test_planning_calculate_acquired_budgets_three_acquisitions_after_end():
         weight=3,
     )
     today = date(2023, 7, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2, acquisition3],
         monthly_budget=60,
         start_budget=0,
@@ -329,7 +341,7 @@ def test_planning_calculate_acquired_budgets_three_acquisitions_after_end():
     assert _sum_acquired_budgets(acquisition1, acquisition2, acquisition3) == 480
 
 
-def test_planning_allocates_start_budget():
+def test_wmcplanning_allocates_start_budget():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -345,7 +357,7 @@ def test_planning_allocates_start_budget():
         weight=2,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2],
         monthly_budget=10,
         start_budget=30,
@@ -359,7 +371,7 @@ def test_planning_allocates_start_budget():
     assert _sum_acquired_budgets(acquisition1, acquisition2) == planning.start_budget
 
 
-def test_planning_allocates_start_budget_applies_extra_budget():
+def test_wmcplanning_allocates_start_budget_applies_extra_budget():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -375,7 +387,7 @@ def test_planning_allocates_start_budget_applies_extra_budget():
         weight=2,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2],
         monthly_budget=10,
         start_budget=30,
@@ -389,7 +401,7 @@ def test_planning_allocates_start_budget_applies_extra_budget():
     assert _sum_acquired_budgets(acquisition1, acquisition2) == planning.start_budget
 
 
-def test_planning_allocate_start_budget_handles_satisfied_acquisitions():
+def test_wmcplanning_allocate_start_budget_handles_satisfied_acquisitions():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -405,7 +417,7 @@ def test_planning_allocate_start_budget_handles_satisfied_acquisitions():
         weight=2,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2],
         monthly_budget=10,
         start_budget=150,
@@ -419,7 +431,7 @@ def test_planning_allocate_start_budget_handles_satisfied_acquisitions():
     assert _sum_acquired_budgets(acquisition1, acquisition2) == 100
 
 
-def test_planning_allocate_start_budget_handles_satisfied_acquisitions_complex():
+def test_wmcplanning_allocate_start_budget_handles_satisfied_acquisitions_complex():
     ac1 = BaseAcquisition(
         name="test1",
         start_budget=0,
@@ -442,7 +454,7 @@ def test_planning_allocate_start_budget_handles_satisfied_acquisitions_complex()
         weight=1,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[ac1, ac2, ac3],
         monthly_budget=10,
         start_budget=200,
@@ -458,7 +470,7 @@ def test_planning_allocate_start_budget_handles_satisfied_acquisitions_complex()
     assert _sum_acquired_budgets(ac1, ac2, ac3) == planning.start_budget + 10
 
 
-def test_planning_uses_start_budgets_before_first_planning_date():
+def test_wmcplanning_uses_start_budgets_before_first_planning_date():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=50,
@@ -467,7 +479,7 @@ def test_planning_uses_start_budgets_before_first_planning_date():
         weight=1,
     )
     today = date(2023, 1, 1)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition],
         monthly_budget=10,
         start_budget=0,
@@ -478,7 +490,7 @@ def test_planning_uses_start_budgets_before_first_planning_date():
     assert acquisition.start_budget == 50
 
 
-def test_planning_allocates_start_budget_long_term():
+def test_wmcplanning_allocates_start_budget_long_term():
     acquisition = BaseAcquisition(
         name="test",
         start_budget=50,
@@ -487,7 +499,7 @@ def test_planning_allocates_start_budget_long_term():
         weight=1,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition],
         monthly_budget=10,
         start_budget=0,
@@ -498,7 +510,7 @@ def test_planning_allocates_start_budget_long_term():
     assert acquisition.start_budget == 50
 
 
-def test_planning_end_to_end_simple():
+def test_wmcplanning_end_to_end_simple():
     acquisition1 = BaseAcquisition(
         name="test1",
         start_budget=50,
@@ -514,7 +526,7 @@ def test_planning_end_to_end_simple():
         weight=4,
     )
     today = date(2023, 3, 15)
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=[acquisition1, acquisition2],
         monthly_budget=50,
         start_budget=100,
@@ -572,7 +584,7 @@ def test_end_to_end_complex():
     )
     today = date(2023, 3, 24)
     acs = [ac1, ac2, ac3, ac4, ac5]
-    planning = BasePlanning(
+    planning = BasePlanningWeightedMonthlyContribution(
         acquisitions=acs,
         monthly_budget=328.64,
         start_budget=1000,
@@ -586,3 +598,91 @@ def test_end_to_end_complex():
     assert_round(ac5.budget_acquired, 1000 / 16 + 275 / 6)
     assert _sum_start_budgets(*acs) == 50
     assert _sum_acquired_budgets(*acs) == 1000 + 328.64 + 50
+
+
+# dated sequential acquisition
+def test_dsaplanning():
+    ac1 = BaseAcquisition(
+        name="test1",
+        start_budget=50,
+        target_budget=200,
+        start_date=date(2022, 12, 1),
+        weight=1,
+    )
+    ac2 = BaseAcquisition(
+        name="test2",
+        start_budget=100,
+        target_budget=500,
+        start_date=date(2023, 1, 1),
+        weight=3,
+    )
+    ac3 = BaseAcquisition(
+        name="test3",
+        start_budget=0,
+        target_budget=100,
+        start_date=date(2023, 1, 1),
+        weight=2,
+    )
+    today = date(2023, 3, 15)
+    planning = BasePlanningDatedSequentialAcquisition(
+        acquisitions=[ac1, ac2, ac3],
+        monthly_budget=100,
+        start_budget=100,
+        today=today,
+    )
+    planning.calculate_acquired_budgets()
+    assert ac1.budget_acquired == 200
+    assert ac2.budget_acquired == 450
+    assert ac3.budget_acquired == 0
+    assert ac1.start_budget == 50
+    assert ac2.start_budget == 100
+    assert ac3.start_budget == 0
+    assert (
+        _sum_acquired_budgets(ac1, ac2)
+        == ac1.start_budget
+        + ac2.start_budget
+        + ac3.start_budget
+        + planning.start_budget
+        + 100 * 4
+    )
+
+
+def test_dsaplanning_get_acquisition_sequence():
+    a1 = ac("b", 1000, 1, 1)
+    a2 = ac("c", 1000, 1, 1)
+    a3 = ac("a", 1000, 1, 1)
+    a4 = ac("b", 750, 1, 1)
+    a5 = ac("b", 1250, 1, 1)
+    a6 = ac("b", 1000, 2, 1)
+    a7 = ac("b", 1000, 0, 1)
+    a8 = ac("b", 1000, 1, 0)
+    a9 = ac("b", 1000, 1, 2)
+    a10 = ac("a", 750, 2, 2)
+
+    expected = [a8, a6, a4, a3, a1, a2, a5, a7, a10, a9]
+    planning = BasePlanningDatedSequentialAcquisition(
+        [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10], 0, 0
+    )
+    result = planning.get_acquisition_sequence()
+    assert result == expected
+
+
+# weighted sequential acquisition
+def test_wsaplanning_get_acquisition_sequence():
+    a1 = ac("b", 1000, 1, 1)
+    a2 = ac("c", 1000, 1, 1)
+    a3 = ac("a", 1000, 1, 1)
+    a4 = ac("b", 750, 1, 1)
+    a5 = ac("b", 1250, 1, 1)
+    a6 = ac("b", 1000, 2, 1)
+    a7 = ac("b", 1000, 0, 1)
+    a8 = ac("b", 1000, 1, 0)
+    a9 = ac("b", 1000, 1, 2)
+    a10 = ac("a", 750, 2, 2)
+
+    expected = [a10, a6, a4, a8, a3, a1, a2, a9, a5, a7]
+    planning = BasePlanningWeightedSequentialAcquisition(
+        [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10], 0, 0
+    )
+    result = planning.get_acquisition_sequence()
+    assert result == expected
